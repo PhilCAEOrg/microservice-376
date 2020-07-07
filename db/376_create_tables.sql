e4d22247c6e74fa410f57cf23e5c05de0e222386
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-07 11:22:56 +0000

    Microservice Init

---
+++ db/376_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 376, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab.
+--
+CREATE TABLE caeschema.tab (
+  id INTEGER ,
+  item VARCHAR(255) ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
